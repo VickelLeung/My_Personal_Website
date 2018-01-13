3affe206637f811dd924c76675152f2267b42254
lunchBoxe <Vickel1993@Hotmail.com>
2018-01-13 13:39:47 -0500

    Added php message box

---
+++ website/mail_handler.php
@@ -0,0 +1,20 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['message'];
+
+		$to='vickel1993@hotmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Your email was Sent Successfully! Thank you"." ".$name.", We will contact ASAP!</h1>";
+		}
+		else{
+			echo "Oh no, something went wrong!";
+		}
+	}
+?>
